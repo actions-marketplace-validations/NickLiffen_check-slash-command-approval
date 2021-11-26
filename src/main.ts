232baad5bca4e36e016f9b77b9a0d68f13835362
Nick Liffen <nickliffen@github.com>
2021-11-26 17:04:49 +0000

    Testing Response

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,24 @@ import { load } from "js-yaml";
 
 const run = async (): Promise<void> => {
   const user = core.getInput("commandUser", { required: true });
+  const response = core.getBooleanInput("response", { required: true });
   const file = core.getInput("fileURI", { required: true });
+
+  console.log(
+    `This action received the following as the user who approved the slash command: ${user}`
+  );
+  console.log(
+    `This action received the following as the response (true/false) to the slash command: ${response}`
+  );
+  console.log(
+    `This action received the following as the path where the approved users can befound: ${file}`
+  );
+
+  if (response === false) {
+    console.log("Trial has been set to NOT APPROVED.");
+    core.setOutput("approved", "false");
+    return;
+  }
 
   const doc = load(readFileSync(file, "utf8"), {
     json: true,
@@ -13,8 +30,10 @@ const run = async (): Promise<void> => {
   const approved = doc.find((region) => region.approvers.includes(`@${user}`));
 
   if (approved) {
+    console.log("Trial has been set to APPROVED by an APPROVED user");
     core.setOutput("approved", "true");
   } else {
+    console.log("Trial has been set to APPROVED by an NON APPROVED user");
     core.setOutput("approved", "false");
   }
 };

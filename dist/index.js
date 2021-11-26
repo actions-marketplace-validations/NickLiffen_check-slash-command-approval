4667dcff40c7f0a5a3baea1c024a29d333a8778b
Nick Liffen <nickliffen@github.com>
2021-11-26 17:06:33 +0000

    Updating ID

--- dist/index.js
+++ dist/index.js
@@ -5693,15 +5693,26 @@ const fs_1 = __nccwpck_require__(147);
 const js_yaml_1 = __nccwpck_require__(917);
 const run = async () => {
     const user = core.getInput("commandUser", { required: true });
+    const response = core.getBooleanInput("response", { required: true });
     const file = core.getInput("fileURI", { required: true });
+    console.log(`This action received the following as the user who approved the slash command: ${user}`);
+    console.log(`This action received the following as the response (true/false) to the slash command: ${response}`);
+    console.log(`This action received the following as the path where the approved users can befound: ${file}`);
+    if (response === false) {
+        console.log("Trial has been set to NOT APPROVED.");
+        core.setOutput("approved", "false");
+        return;
+    }
     const doc = (0, js_yaml_1.load)((0, fs_1.readFileSync)(file, "utf8"), {
         json: true,
     });
     const approved = doc.find((region) => region.approvers.includes(`@${user}`));
     if (approved) {
+        console.log("Trial has been set to APPROVED by an APPROVED user");
         core.setOutput("approved", "true");
     }
     else {
+        console.log("Trial has been set to APPROVED by an NON APPROVED user");
         core.setOutput("approved", "false");
     }
 };

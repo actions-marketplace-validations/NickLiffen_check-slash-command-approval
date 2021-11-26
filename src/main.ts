5452e6a467068efd063e734333d9bc273a1f6fd9
Nick Liffen <nickliffen@github.com>
2021-11-26 15:40:04 +0000

    First Basic Version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-/* Edit Function */
+import * as core from "@actions/core";
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
+
 const run = async (): Promise<void> => {
-  return;
+  const user = core.getInput("commandUser", { required: true });
+  const file = core.getInput("fileURI", { required: true });
+
+  const doc = load(readFileSync(file, "utf8"), {
+    json: true,
+  }) as file;
+
+  const approved = doc.find((region) => region.approvers.includes(user));
+
+  if (approved) {
+    core.setOutput("approved", "true");
+  } else {
+    core.setOutput("approved", "false");
+  }
 };
 
 run();
